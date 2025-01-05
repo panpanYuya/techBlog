a0573306d9b9329fce85d583acf76af29abf3af4
panpanYuya <yuya.poko.0224@gmail.com>
2025-01-06 01:41:33 +0900

    次のデプロイではまだ未実装予定のため、一度コードを削除

--- front/src/components/search/search.tsx
+++ front/src/components/search/search.tsx
@@ -9,12 +9,6 @@ function Search() {
                 placeholder="タグで検索..."
                 className="search-input"
             />
-            <select className="group-select">
-                <option value="">グループを選択</option>
-                <option value="group1">グループ1</option>
-                <option value="group2">グループ2</option>
-                <option value="group3">グループ3</option>
-            </select>
         </div>
     );
 }
