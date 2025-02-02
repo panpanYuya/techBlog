85d48ef99492fd4210ff1c8b48a47254f3d3999c
panpanYuya <yuya.poko.0224@gmail.com>
2025-02-02 23:59:55 +0900

    セキュリティ情報を削除

--- front/src/firebase.ts
+++ front/src/firebase.ts
@@ -2,14 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCN28EyWeyIUaT6d9RHRmOKohqZJiyzFPw",
-    authDomain: "powerup-blog.firebaseapp.com",
-    projectId: "powerup-blog",
-    storageBucket: "powerup-blog.appspot.com",
-    messagingSenderId: "1062903502777",
-    appId: "1:1062903502777:web:ccf6b93d8d1058a7764868",
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
